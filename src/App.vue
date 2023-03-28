82b241c9287e888e055cee25738139402f58cbd4
tog22 <tom.ash.github@philosofiles.com>
2023-03-27 18:43:55 -0700

    🔥  *HOWTO* Add Firebase!

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,82 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, provide } from 'vue'
+import firebase_messaging from './firebase'
+
 import '@/assets/styles.css'
 
 export default defineComponent({
 	name: 'App',
 	setup() {
+
+		/******************
+		**  🔥 FIREBASE  **
+		******************/
+
+		let get_token = firebase_messaging.getToken({vapidKey: "BACyAFjs1KoHzgCkmXllHlmBBqj6yLbxcJSD4wjxjN-bJKl6zaWSevcaxkanK0RD05GJrPK-1yHodls6kGoaf4w"});
+
+		get_token.then(
+			function (result) {
+				
+				// store.token = result.token
+
+				lo('🔥 FCM registration token:')
+				lo(result)
+				
+				// if (store.logged_in) {
+				// 	var server_request = new XMLHttpRequest()
+
+				// 	let get_url = 'http://godcloud.philosofiles.com/?action=report_token&token='+result.token+'&user='+store.user;
+				// 	lo(get_url);
+
+				// 	server_request.open("GET", get_url, false) // false = synchronous
+				// 	server_request.send()
+				// }
+
+			},
+			function (error) {
+				lo('🔥 FCM: getting token')
+			}
+		);
+
+		firebase_messaging.onMessage((message) => {
+			alert('FCM received ')
+			console.log('FCM message = ',message)
+			// let msg_body = fcm_body_to_object(message.notification.body)
+			// switch (message.notification.title) {
+			// 	case 'move':
+			// 		// bus.$emit('move', msg_body)
+			// 		console.log('move', msg_body)
+			// 		break
+			// 	default: { // {} to allow `let`
+			// 		let alert_text = 'Unknown firebase message received: '+JSON.stringify(message.notification)
+			// 		alert(alert_text)
+			// 		break
+			// 	}
+			// }
+		})
+
+		provide('messaging', firebase_messaging)
+
 	}
 })
+
+/****************
+**	FUNCTIONS  **
+*****************/
+
+function fcm_body_to_object(string) {
+	
+	string.replace('\"', '"')  // eslint-disable-line
+	let object = JSON.parse(string)
+	return object
+	
+}
+
+function lo(to_log) { 
+	console.log(to_log) 
+}
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,37 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/******************
+**			     **
+**  🔥 FIREBASE  **
+**			     **
+******************/
+
+// Service worker part
+
+// Register service worked in standard vanillla JS way
+// 
+// (Alt method at https://dev.to/vbanditv/how-to-add-fcm-firebase-cloud-messaging-to-vuejs-37np)
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('firebase-messaging-sw.js')
+	  .then(reg => {
+		console.log(`Service Worker Registered (Scope: ${reg.scope})`);
+	  })
+	  .catch(error => {
+		const msg = `Service Worker Error (${error})`;
+		console.error(msg);
+	  });
+  } else {
+	// happens when the app isn't served over HTTPS or if the browser doesn't support service workers
+	console.warn('Service Worker not available');
+  }
+  
+
+/*****************
+**				**
+**  VUE BASICS  ** 
+**				**
+*****************/
+
 createApp(App).use(router).mount('#app')
