1757f718a71031ddfc3711f51cd47d79c9101f43
tog22 <tom.ash.github@philosofiles.com>
2023-04-07 13:56:50 -0700

    Page: Online Games Menu

--- src/views/Online_Games_Menu.vue
+++ src/views/Online_Games_Menu.vue
@@ -3,85 +3,154 @@
 		<div id="selecting_online" class="screen">
 			<Menu_Bar />
 			<div id="inner_selecting_online">
-
-				<h1 class="page_title">
-					Play Online
-				</h1>
 				
-				<div id="menu_buttons">
+				<div v-if="subscreen === 'Initial menu'">
 					
-					<div v-if="!store.online.user" class="button" @click="to_log_in">
-						Log In
-					</div>
+					<h1 class="page_title">
+						Play Online
+					</h1>
 
-					<div v-if="!store.online.user" class="button" @click="to_sign_up">
-						Sign Up
-					</div>
-
-					<div v-else>
+					<div id="menu_buttons">
 						
-						<div v-if="store.online.subscreen === 'user menu' ">
-							<div class="button" @click="new_online">
+						<div v-if="store.online.user">
+			
+							<router-link 
+								class="button" 
+								to="/new_online_game"
+							>
 								New Game
-							</div>
-							<div class="button" @click="continue_online">
+							</router-link>
+
+							<router-link 
+								class="button" 
+								to="/continue_game"
+							>
 								Continue Game
-							</div>
-							<div class="button" @click="logout">
+							</router-link>
+
+							<div 
+								@click="logout"
+								class="button" 
+							>
 								Log out
-							</div>
-						</div>
-
-						<div v-else-if="store.online.subscreen === 'select opponent' ">
-							<div class="form">
-								<div class="input_with_label">
-									<div class="s_label">
-										Opponent's username
-									</div>
-									<div class="s_input">
-										<input name="opponent" id="newg_opponent" type="text" required />
-									</div>
-								</div>
-								<div class="s_input">
-									<button type="button" @click="select_opponent">
-										Start game
-									</button>
-								</div>
-								<div class="form_error">
-									{{store.online.error}}
-								</div>
-							</div>
-						</div>
-
-						<div v-else-if="store.online.subscreen === 'continue online' ">
-
-							<div v-if="store.online.has_current_games">
-								<div
-									v-for="(listed_game, lg_index) in store.online.games"
-										:key="'lg'+lg_index"
-										@click="load_game(listed_game.id, listed_game.game_pass, listed_game.side)"
-										class="button continue_game_button"
-								>
-									{{listed_game.opponent}}
-								</div>
-							</div>
-
-							<div v-else>
-								<div class="no_current_games">
-									No current games
-								</div>
 							</div>
 
 						</div>
 
 						<div v-else>
-							Error: no recognised online.subscreen set
+
+							<div class="button" @click="to_login">
+								Log In
+							</div>
+							<div class="button" @click="to_signup">
+								Sign Up
+							</div>
+
+						</div>
+
+					</div><!-- /end #menu_buttons-->
+
+				</div>
+
+				<div v-else-if="subscreen === 'Login'">
+					
+					<h1 class="page_title">
+						Log In
+					</h1>
+					
+					<div id="content">
+						
+						<!-- todo: convert this and signup to forms -->
+						<div class="login form">
+							<div class="input_with_label">
+								<div class="s_label">
+									Username
+								</div>
+								<div class="s_input">
+									<input name="user" id="login_user" class="user" type="text" required />
+								</div>
+							</div>
+							<div class="input_with_label">
+								<div class="s_label">
+									Password
+								</div>
+								<div class="s_input">
+									<input name="pass" id="login_pass" class="pass" type="password" required />
+								</div>
+							</div>
+							<div class="s_input">
+								<button type="button" @click="log_in_button()">
+									Log in
+								</button>
+							</div>
+							<div 
+								v-if="error"
+								class="form_error_container"
+							>
+								<div class="form_error">
+									{{error}}
+								</div>
+							</div>
 						</div>
 
 					</div>
 
 				</div>
+
+				<div v-else-if="subscreen === 'Signup'">
+
+					<h1 class="page_title">
+						Sign Up
+					</h1>
+
+					<div id="content">
+						<div class="signup form">
+							<div class="input_with_label">
+								<div class="s_label">
+									Username (no spaces)
+								</div>
+								<div class="s_input">
+									<input name="user" class="user" id="signup_user" type="text" required />
+								</div>
+							</div>
+							<div class="input_with_label">
+								<div class="s_label">
+									Password
+								</div>
+								<div class="s_input">
+									<input name="pass" class="pass" id="signup_pass" type="password" required />
+								</div>
+							</div>
+							<div class="s_input">
+								<button type="button" @click="sign_up_button()">
+									Sign up
+								</button>
+							</div>
+							<div 
+								v-if="error"
+								class="form_error_container"
+							>
+								<div class="form_error">
+									{{error}}
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div v-else>
+
+					<p>
+						Error - unknown subscreen
+					</p>
+
+				</div>
+
 			</div>
+
+				
+				
 		</div>
 	</div>
 </template>
@@ -115,236 +184,107 @@ export default defineComponent({
 	},
 
 	methods: {
-		back_after_end_game() {
-			this.which_screen = 'show_menu'
-			this.win_type = null
-			this.victory_or_defeat =  ''
-			this.type_of_victory =  ''
+		
+		to_login() {
+			this.subscreen = 'Login'
 		},
-		back_from_pnp() {
-			this.back_after_end_game()
+
+		to_signup() {
+			this.subscreen = 'Signup'
 		},
-		back_within_selecting_online() {
+
+		log_in_button() {
 			
-			switch (this.store.online.subscreen) {
-				case 'login or signup':
-				case 'user menu':
-					this.which_screen =  'show_menu'
-					break
-				case 'select opponent':
-				case 'continue online':
-					this.store.online.subscreen = 'user menu'
-					break
-				default:
-					this.which_screen =  'show_menu'
-					break
-			}
-
-			if (this.store.online.user === 'logging_in' || this.store.online.user === 'signing_up') {
-				this.store.online.user = ''
-			}
-
-		},
-		back_from_online() {
-			this.which_screen =  'show_selecting_online'
-		},
-		online_games() {
-			this.which_screen =  'show_selecting_online'
-			this.store.online.subscreen = 'user menu' // is this right? was 'login or signup'
-		},
-		to_log_in() {
-			this.store.online.user = 'logging_in'
-		},
-		to_sign_up() {
-			this.store.online.user = 'signing_up'
-		},
-		logout() {
-			this.store.online.user = ''
-			// todo - remove token from db
-		},
-		debug() {
-			lo(this)
-		},
-		log_in_button(error = false) {
-
-			var user
-			var pw
-			if (error) {
-				user = document.getElementById('log_err_u').value
-				pw = document.getElementById('log_err_p').value
-			} else {
-				user = document.getElementById('log_u').value
-				pw = document.getElementById('log_p').value
-			}
-
+			this.error = false // reset
+			
+			let user = document.getElementById('login_user').value
+			let pw = document.getElementById('login_pass').value
+			
 			var server_request = new XMLHttpRequest()
-
+			
 			let get_url = 'http://godcloud.philosofiles.com/?action=login&username='+user+'&pw='+pw
 			//lo(get_url)
-
+			
 			server_request.open("GET", get_url, false)
 			server_request.send()
-
+			
 			const response = JSON.parse(server_request.responseText)
 			lo(response)
-
+			
 			if (response.result === 'success') {
 				
 				this.store.online.user = user
 				this.store.user = user
 				this.store.online.userpass = pw
-				this.store.online.subscreen = 'user menu'
-
 				this.report_token(user, this.store.token)
-
+				
+				this.subscreen = 'Initial menu'
+				
 			} else if (response.result === "un or pw wrong") {
-				this.which_screen = 'show_selecting_online'
-				this.store.online.user = 'login_error'
-				this.store.online.login_error='Wrong username or password'
+				
+				this.error = 'Incorrect login details'
+				
 			} else {
-				// todo: general error message
-				alert('other error')
+				
+				this.error = 'Error logging in'
+				
 			}
-
+			
 		},
-		sign_up_button(error = false) {
-
-			var user
-			var pw
-			if (error) {
-				user = document.getElementById('su_err_u').value
-				pw = document.getElementById('su_err_p').value
-			} else {
-				user = document.getElementById('su_u').value
-				pw = document.getElementById('su_p').value
-			}
-
+		sign_up_button() {
+			
+			this.error = false // reset
+			
+			let user = document.getElementById('signup_user').value
+			let pw = document.getElementById('signup_pass').value
+			
 			var server_request = new XMLHttpRequest()
-
+			
 			let get_url = 'http://godcloud.philosofiles.com/?action=signup&username='+user+'&pw='+pw
 			//lo(get_url)
-
+			
 			server_request.open("GET", get_url, false)
 			server_request.send()
-
+			
 			const response = JSON.parse(server_request.responseText)
 			lo(response)
-
+			
 			if (response.result === 'success') {
 				
 				this.store.online.user = user
 				this.store.user = user
-				this.store.online.subscreen = 'user menu'
-
 				this.report_token(user, this.store.token)
-
+				
+				this.subscreen = 'Initial menu'
+				
 			} else if (response.result === "sql error") {
-				this.which_screen = 'show_selecting_online'
-				this.store.online.user = 'signup_error'
 				if (response.error_number === '1062') {
-					this.store.online.signup_error='Username already taken'
+					this.error='Username already taken'
 				} else {
-					this.store.online.signup_error='Unknown error'
+					this.error='Unknown SQL error'
 				}
 			} else {
 				// todo: general error message
-				alert('other error')
+				this.error='Unknown non-SQL error'
 			}
-
+			
 		},
 		report_token(user, token) {
 			var server_request = new XMLHttpRequest()
-
+			
 			let get_url = 'http://godcloud.philosofiles.com/?action=report_token&token='+this.store.token+'&user='+user;
 			lo(get_url);
-
+			
 			server_request.open("GET", get_url, false) // false = synchronous
 			server_request.send()
 		},
-		new_online() {
-			this.store.online.subscreen = 'select opponent'
+		online_games() {
+			this.which_screen =  'show_selecting_online'
+			this.store.online.subscreen = 'user menu' // is this right? was 'login or signup'
 		},
-		select_opponent() {
-
-			var opp = document.getElementById('newg_opponent').value
-			var server_request = new XMLHttpRequest()
-
-			let get_url = 'http://godcloud.philosofiles.com/?action=find_user&user='+opp
-			lo(get_url)
-
-			server_request.open("GET", get_url, false)
-			server_request.send()
-
-			const response = JSON.parse(server_request.responseText)
-
-			if (response.result === 'failure') {
-				this.store.online.error = 'Opponent not found'
-			} else if (response.result === 'success') {
-
-				let pw = Math.floor(Math.random() * 32000)
-
-				server_request = new XMLHttpRequest()
-				get_url = 'http://godcloud.philosofiles.com/?action=create&pw='+pw+'&p1='+this.store.online.user+'&p2='+opp
-
-				server_request.open("GET", get_url, false)
-				server_request.send()
-
-				const response = JSON.parse(server_request.responseText)
-
-				if (response.result === 'success') {
-					this.game_type = 'online'
-					this.which_screen =  'show_online'
-					this.store.online.side =  1
-					this.store.online.game_id = response.game_id
-					this.store.online.game_pass = pw
-				} else {
-					this.store.online.error = 'Failed to create game'
-				}
-
-			} else {
-				this.store.online.error = 'Unknown error'
-			}
-
-		},
-		continue_online() {
-
-			let server_request = new XMLHttpRequest()
-			let get_url = 'https://godcloud.philosofiles.com/?action=list_games&username='+this.store.online.user+'&userpass='+this.store.online.userpass
-			lo(get_url)
-
-			server_request.open("GET", get_url, false)
-			server_request.send()
-
-			const response = JSON.parse(server_request.responseText)
-
-			if (response.result === 'failure' || !response.games_found) {
-				alert("You don't have any active games")
-				return
-				// todo: change
-			}
-
-			this.store.online.games = response.games
-			this.store.online.subscreen = 'continue online'
-
-		},
-		load_game(id, pw, side) {
-			this.game_type = 'online'
-			this.which_screen =  'show_online'
-			this.store.online.game_id = id
-			this.store.online.game_pass = pw
-			this.store.online.side =  side
-		},
-		new_pass_and_play() {
-			this.game_type = 'pnp'
-			bus.emit('change_gameworld_data', {'online_game': false})
-			this.which_screen =  'show_pnp'
-		},
-		test_online_game() {
-			this.game_type = 'online'
-			this.which_screen =  'show_online'
-		},
-		test_move_msg() {
-			bus.emit('test_move_arrow', {})
+		logout() {
+			this.store.online.user = ''
+			// todo - remove token from db
 		}
 	},
 
@@ -352,7 +292,9 @@ export default defineComponent({
 		const store_parent = inject("store")
 		
 		return {
-			store: store_parent.state
+			store: 		store_parent.state,
+			subscreen:	'Initial menu',
+			error:		false
 		}
 	}
 })
