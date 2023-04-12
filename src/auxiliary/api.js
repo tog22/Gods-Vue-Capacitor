d1122232b8b2d10539e2e762ba349ce22587c89f
tog22 <tom.ash.github@philosofiles.com>
2023-04-12 10:01:28 -0700

    Update api.js

--- src/auxiliary/api.js
+++ src/auxiliary/api.js
@@ -9,56 +9,67 @@ const api = axios.create({
 
 // defining a custom error handler for all APIs
 const errorHandler = (error) => {
-  const statusCode = error.response?.status
-
-  // logging only errors that are not 401
-  if (statusCode && statusCode !== 401) {
-    console.error(error)
-  }
-
-  return Promise.reject(error)
+	const statusCode = error.response?.status
+	
+	// logging only errors that are not 401
+	if (statusCode && statusCode !== 401) {
+		console.error(error)
+	}
+	
+	return Promise.reject(error)
 }
 
 // registering the custom error handler to the
 // "api" axios instance
 api.interceptors.response.use(undefined, (error) => {
-  return errorHandler(error)
+	return errorHandler(error)
 })
 
-export function defineCancelApiObject(apiObject) {
-    // an object that will contain a cancellation handler
-    // associated to each API property name in the apiObject API object
-    const cancelApiObject = {}
-
-    // each property in the apiObject API layer object
-    // is associated with a function that defines an API call
-
-    // this loop iterates over each API property name
-    Object.getOwnPropertyNames(apiObject).forEach((apiPropertyName) => {
-        const cancellationControllerObject = {
-            controller: undefined,
-        }
-
-        // associating the request cancellation handler with the API property name
-        cancelApiObject[apiPropertyName] = {
-            handleRequestCancellation: () => {
-                // if the controller already exists,
-                // canceling the request
-                if (cancellationControllerObject.controller) {
-                    // canceling the request and returning this custom message
-                    cancellationControllerObject.controller.abort()
-                }
-
-                // generating a new controller
-                // with the AbortController factory
-                cancellationControllerObject.controller = new AbortController()
-
-                return cancellationControllerObject.controller
-            },
-        }
-    })
-
-    return cancelApiObject
+function defineCancelApiObject(apiObject) {
+	// an object that will contain a cancellation handler
+	// associated to each API property name in the apiObject API object
+	const cancelApiObject = {}
+	
+	// each property in the apiObject API layer object
+	// is associated with a function that defines an API call
+	
+	// this loop iterates over each API property name
+	Object.getOwnPropertyNames(apiObject).forEach((apiPropertyName) => {
+		const cancellationControllerObject = {
+			controller: undefined,
+		}
+		
+		// associating the request cancellation handler with the API property name
+		cancelApiObject[apiPropertyName] = {
+			handleRequestCancellation: () => {
+				// if the controller already exists,
+				// canceling the request
+				if (cancellationControllerObject.controller) {
+					// canceling the request and returning this custom message
+					cancellationControllerObject.controller.abort()
+				}
+				
+				// generating a new controller
+				// with the AbortController factory
+				cancellationControllerObject.controller = new AbortController()
+				
+				return cancellationControllerObject.controller
+			},
+		}
+	})
+	
+	return cancelApiObject
 }
 
-export default api
+export default const godcloud = {
+	get: async function (url, cancel = false) {
+		const response = await api.request({
+			url: 		url,
+			method: 	"GET",
+			// ↓ retrieving the signal value by using the property name
+			signal: 	cancel ? cancelApiObject[this.get.name].handleRequestCancellation().signal : undefined,
+		})
+		
+		return response
+	},
+}
